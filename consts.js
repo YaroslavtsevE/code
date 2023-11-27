bfd97ac47b247dccdc712dcf618be4d2390be55d
Evgeny <yaroslavtseveugene@gmail.com>
2023-11-27 21:54:27 +0700

    added toyota finder

--- consts.js
+++ consts.js
@@ -5,5 +5,13 @@ export default {
     HEADER_REGION: 'Новосибирская область',
     NAV_AUTO: 'div > [data-ftid="component_header_main-menu-item"]:first-child',
     NAV_AUTO_TEXT: 'Автомобили',
-    DROP_BOX_MODEL: '[placeholder="Марка"]'
+    DROPBOX_MODEL: '[placeholder="Марка"]',
+    DROPBOX_MODEL_TOYOTA: '[data-ftid="component_select_dropdown"] > div > div:nth-child(3) > div',
+    DROPBOX_MODEL_TOYOTA_TEXT: 'Toyota',
+    INPUT_PRICE_FROM: '[placeholder="Цена от, ₽"]',
+    INPUT_PRICE_FROM_VALUE: '700000',
+    INPUT_PRICE_TO: '[placeholder="до"]',
+    INPUT_PRICE_TO_VALUE: '730000',
+    CHECKBOX_PHOTO: '[id="photo"]',
+    BUTTON_SUBMIT: '[type="submit"]'
   };
--- test.cy.js
+++ test.cy.js
@@ -12,7 +12,14 @@ describe("drom", function () {
     cy.get(consts.HEADER_REGION_ELEMENT).should('have.text',consts.HEADER_REGION)
     cy.get(consts.NAV_AUTO).should('have.text',consts.NAV_AUTO_TEXT).click()
     cy.get(consts.NAV_AUTO).should('have.text',consts.NAV_AUTO_TEXT).click()
-    cy.get(consts.DROP_BOX_MODEL).click()
+    cy.get(consts.DROPBOX_MODEL).click()
     cy.wait(parameters.WAITING)
+    cy.get(consts.DROPBOX_MODEL_TOYOTA).should('include.text',consts.DROPBOX_MODEL_TOYOTA_TEXT).click()
+    cy.wait(parameters.WAITING)
+    cy.get(consts.INPUT_PRICE_FROM).type(consts.INPUT_PRICE_FROM_VALUE)
+    cy.get(consts.INPUT_PRICE_TO).type(consts.INPUT_PRICE_TO_VALUE)
+    cy.get(consts.CHECKBOX_PHOTO).click({ force: true })
+    cy.get(consts.CHECKBOX_PHOTO).should('be.checked')
+    cy.get(consts.BUTTON_SUBMIT).click()
   });
 });
